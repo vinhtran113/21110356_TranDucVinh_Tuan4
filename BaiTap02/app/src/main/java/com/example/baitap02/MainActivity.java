6ea8f1fee983a5801720cd007271aeb90f6f4b92
vinhtran113 <tvih6693@gmail.com>
2024-03-25 14:57:46 +0700

    upp

---
+++ BaiTap02/app/src/main/java/com/example/baitap02/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.baitap02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.os.CountDownTimer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout bg= (ConstraintLayout) findViewById(R.id.constraintLayout1);
+
+        //switch
+        Switch sw = findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){ //isChecked = true
+                    bg.setBackgroundResource(R.drawable.bg1);
+                }else{
+                    bg.setBackgroundResource(R.drawable.bg3);
+                }
+            }
+        });
+    }
+
+}
